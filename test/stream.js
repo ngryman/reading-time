f1e0220ea6dd5240b22d16e53b96929d284950e2
ngryman <ngryman@gmail.com>
2015-07-03 19:33:43 +0200

    stream support

--- test/stream.js
+++ test/stream.js
@@ -9,7 +9,7 @@
 /**
  * Module dependencies.
  */
-var readingTime = require('..'),
+var readingTime = require('../lib/stream'),
 	curry = require('curry'),
 	chai = require('chai');
 
@@ -20,43 +20,57 @@ chai.should();
  */
 
 var testTime = curry(function(words, expect, done) {
-	var text;
+	var chunks;
 	if ('number' == typeof words)
-		text = generateText(words);
+		chunks = generateChunks(words);
 	else {
-		text = words;
+		chunks = [new Buffer(words)];
 		words = 3;
 	}
 
-	var res = readingTime(text);
-	res.should.have.property('text', expect.text);
-	res.should.have.property('words', words);
-	// floating point funkyness
-	//   http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
-	res.should.have.property('time').that.is.within(expect.time - 0.000001, expect.time + 0.000001);
+	var analyzer = readingTime();
+	analyzer.on('data', function(res) {
+		res.should.have.property('text', expect.text);
+		res.should.have.property('words', words);
+		// floating point funkyness
+		//   http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+		res.should.have.property('time').that.is.within(expect.time - 0.000001, expect.time + 0.000001);
 
-	done();
+		done();
+	});
+
+	chunks.forEach(analyzer.write.bind(analyzer));
+	analyzer.end();
 });
 
-function generateText(words) {
+function generateChunks(words) {
 	var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789àâéèêôùûçÀÂÉÈÔÙÛÇ',
 		charsLength = chars.length,
-		text = '';
+		chunks = [],
+		chunk = '';
 
 	for (var i = 0; i < words; i++) {
 		var wordLength = Math.ceil(Math.random() * 10);
 		for (var j = 0; j < wordLength; j++)
-			text += chars[Math.floor(Math.random() * charsLength)];
-		text += ' ';
-	}
+			chunk += chars[Math.floor(Math.random() * charsLength)];
+		chunk += ' ';
 
-	return text;
+		if (0 !== i && 0 === i % 10) {
+			chunks.push(chunk);
+			chunk = '';
+		}
+	}
+	chunks.push(chunk);
+
+	return chunks.map(function(chunk) {
+		return new Buffer(chunk);
+	});
 }
 
 /**
  * Test suite.
  */
-describe('readingTime()', function() {
+describe('readingTime stream', function() {
 
 	it('should handle less than 1 minute text', testTime(2, {
 		text: '1 min read',
