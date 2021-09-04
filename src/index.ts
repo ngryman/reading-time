ed8c45807c033175febfb4afbc135e26a0b329a0
Josh-Cena <sidachen2003@gmail.com>
2021-09-04 16:34:34 +0800

    Better import/export
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import readingTime from './reading-time'
 import ReadingTimeStream from './stream'
 
-export { ReadingTimeStream }
-export default readingTime
-/** Wacky way to support const readingTime = require('reading-time') */
+// Wacky way to support const readingTime = require('reading-time') :(
+// Basically we can't use ES import/export anymore because re-assigning module.exports
+// decouples it from the exports object, which TS export compiles to
 module.exports = readingTime
+module.exports.default = readingTime
+module.exports.ReadingTimeStream = ReadingTimeStream
